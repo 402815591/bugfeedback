2949ccbc6eb8778dadd0a2a0d69d492bdb3344db
lzh <l402815591@sina.com>
2016-10-28 15:55:18 +0800

    多文件上传

--- index/models.py
+++ index/models.py
@@ -12,12 +12,18 @@ from mongoengine.base import BaseField
 from mongoengine.fields import IntField, StringField
 from django.db.models.fields.files import FieldFile
 
+
 connect('test2', host='192.168.100.201', port=27017)
+
+class Imgfiles(EmbeddedDocument):
+    img_content = FileField(collection_name='fs')
+    # img_content2 = FileField(collection_name='fs')
+    # img_content3 = FileField(collection_name='fs')
 
 
 class bug(Document):
     question = StringField()
     content = StringField(max_length=300)
-    image = FileField(collection_name='fs')
+    image = ListField(EmbeddedDocumentField('Imgfiles'))
     contract_way = StringField()
     contract = IntField(max_length=15)
--- index/views.py
+++ index/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render
 from models import  bug
+from models import  Imgfiles
 # Create your views here.
+
 
 def index(request):
     return render(request, 'inex.html', {})
-
 
 def addcotant(request):
         content = request.POST["content"]
         contract = request.POST["contract"]
         check_box_list = request.REQUEST.getlist('check_box_list')
         select = request.POST["contract_way"]
-        img = request.FILES['photo']
+        img = request.FILES.getlist('photo')
+        # print img
         feedback = bug()
         feedback.content = content
         feedback.contract = contract
         question = ','.join(check_box_list)
         feedback.question = question
-        image = img
-        feedback.image.put(image, filename=img._name)
         feedback.contract_way = select
+
+        for f in img:
+            print f, f._name
+            feedbackimg = Imgfiles()
+            feedbackimg.img_content.put(f, filename=f._name)
+            feedback.image.append(feedbackimg)
         feedback.save()
-
         return  render(request, 'inex.html', {'feedback': feedback})
-
