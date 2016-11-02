2973709a40adbde34cb047916c483e522dede9e9
lzh <l402815591@sina.com>
2016-11-02 15:40:08 +0800

    校验完成

--- index/views.py
+++ index/views.py
@@ -15,7 +15,6 @@ class check_forms(forms.Form):
     content = forms.CharField(error_messages={"required": "反馈内容错误", })
     contract = forms.IntegerField(error_messages={"required": "联系方式错误", })
     contract_way = forms.CharField(error_messages={"required": "联系方式错误", })
-    # question = forms.CharField(error_messages={"required": "反馈的问题", })
 
 
 
@@ -27,7 +26,6 @@ class check_forms(forms.Form):
             print 'except: ' + str(e)
             raise forms.ValidationError(u"建议内容不能为空")
 
-
         try:
             contract = self.cleaned_data['contract']
         except Exception as e:
@@ -35,12 +33,12 @@ class check_forms(forms.Form):
             raise forms.ValidationError(u"联系方式不能为空")
 
         try:
-            contract_way = self.cleaned_data['contract_way']
-            # question = ','.join(self.data['check_box_list'])
-            # assert bool(question), 'Not empty'
+
+            question = self.data['check_box_list']
+            print question
         except Exception as e:
             print 'except: ' + str(e)
-            raise forms.ValidationError(u"选择的问题不能未空")
+            raise forms.ValidationError(u"反馈的问题不能为空")
 
         return self.cleaned_data
 
@@ -56,7 +54,6 @@ def addcotant(request):
             print str(e),
 
         if feedback_form.is_valid():
-            print "1212121"
             check_box_list = request.REQUEST.getlist('check_box_list')
             if check_box_list:
                 print "check success"
